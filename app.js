151687d11b4972e815d09ce8dcfa1e38b61b6fe7
aurigadl <aurigadl@gmail.com>
2013-02-14 06:14:09 -0500

    class pass

--- app.js
+++ app.js
@@ -62,37 +62,6 @@ hash('foobar', function(err, salt, hash){
 
 // Authenticate using our plain-object database of doom!
 
-function authenticate(name, pass, fn) {
-  if (!module.parent){
-    console.log('authenticating %s:%s', name, pass);
-  }
-  var user = users[name];
-  // query the db for the given username
-  if (!user){
-    return fn(new Error('cannot find user'));
-  }
-  // apply the same algorithm to the POSTed password, applying
-  // the hash against the pass / salt, if there is a match we
-  // found the user
-  hash(pass, user.salt, function(err, hash){
-    if (err){
-      return fn(err);
-    }
-    if (hash === user.hash){
-      return fn(null, user);
-    }
-    fn(new Error('invalid password'));
-  });
-}
-
-function restrict(req, res, next) {
-  if (req.session.user) {
-    next();
-  } else {
-    req.session.error = 'Access denied!';
-    res.redirect('/login');
-  }
-}
 
 require("./routes")(app);
 
